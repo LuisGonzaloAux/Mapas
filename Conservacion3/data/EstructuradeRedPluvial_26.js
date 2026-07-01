095ea823dd563976120e578818e1e613d025c909
LuisGonzaloAux <auxiliarsanrafaelii@gmail.com>
2026-07-01 08:53:59 -0500

    Delete Conservacion3/data directory

--- Conservacion3/data/EstructuradeRedPluvial_26.js
+++
@@ -1,1 +0,0 @@
-var json_EstructuradeRedPluvial_26 = {"type":"FeatureCollection","name":"EstructuradeRedPluvial_26","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
